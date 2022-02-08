8f0951e5cd36d22b96c250060ad2062e5c2224cb
Kev. Noel <arita37@users.noreply.github.com>
2022-02-08 22:18:18 +0900

    Update util_pd_to_polars.py

--- util_pd_to_polars.py
+++ util_pd_to_polars.py
@@ -2,7 +2,7 @@ import polars as pl
 
 ###################################################################################################
 ###### Polars #####################################################################################
-def pl_split(df,  col='colstr', sep=",",  colnew="colstr_split", ):
+def pd_split(df,  col='colstr', sep=",",  colnew="colstr_split",   mode='pandas' ):
     """
       dfp['new1'] = dfp.apply(lambda x :  min( x['str1'].split(","))   , axis=1)
       df['new1'] = df.select(["*",  pl.col("str1").apply(lambda x : min(x.split(",")) ).alias("booknew")])['booknew']
@@ -13,15 +13,16 @@ def pl_split(df,  col='colstr', sep=",",  colnew="colstr_split", ):
 
 
 
-def pl_groupby_join(df,  colgroup="colgroup", col='colstr', sep=",",   ):
+def pd_groupby_join(df,  colgroup="colgroup", col='colstr', sep=",",   mode='pandas' ):
     """
       dfp['new1'] = dfp.apply(lambda x :  min( x['str1'].split(","))   , axis=1)
       df['new1']  = df.select(["*",  pl.col("str1").apply(lambda x : min(x.split(",")) ).alias("booknew")])['booknew']
 
     """
-    df.groupby(colgroup ).agg(pl.col(col)).select([pl.col(colgroup ), pl.col(col).arr.join(sep)])
-    return df
-
+    if str(mode) == 'polars' :
+       return df.groupby(colgroup ).agg(pl.col(col)).select([pl.col(colgroup ), pl.col(col).arr.join(sep)])
+   
+    return dfp.groupby([ colgroup ]).apply(lambda x: f"{sep}".join(x[ col ].values))
 
 
 
