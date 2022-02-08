42bddf4d20b258e163c5852b0034bb9ee643f718
Kev. Noel <arita37@users.noreply.github.com>
2022-02-08 21:59:06 +0900

    Update and rename util_pd_to_poalrs.py to util_pd_to_polars.py

---
+++ util_pd_to_polars.py
@@ -0,0 +1,77 @@
+import polars as pl
+
+###################################################################################################
+###### Polars #####################################################################################
+def pl_split(df,  col='colstr', sep=",",  colnew="colstr_split", ):
+    """
+      dfp['new1'] = dfp.apply(lambda x :  min( x['str1'].split(","))   , axis=1)
+      df['new1'] = df.select(["*",  pl.col("str1").apply(lambda x : min(x.split(",")) ).alias("booknew")])['booknew']
+
+    """
+    df[ colnew ] = df.select(["*",  pl.col(col).apply(lambda x : x.split(",") ).alias(colnew )])[colnew ]
+    return df
+
+
+
+def pl_groupby_join(df,  colgroup="colgroup", col='colstr', sep=",",   ):
+    """
+      dfp['new1'] = dfp.apply(lambda x :  min( x['str1'].split(","))   , axis=1)
+      df['new1']  = df.select(["*",  pl.col("str1").apply(lambda x : min(x.split(",")) ).alias("booknew")])['booknew']
+
+    """
+    df.groupby(colgroup ).agg(pl.col(col)).select([pl.col(colgroup ), pl.col(col).arr.join(sep)])
+    return df
+
+
+
+
+
+def test2():
+    nmin = 2
+    nmax=5000
+    # df = pd_create_random(nmax=5000000)
+    df = pd.DataFrame()
+    df['key'] = np.arange(0, nmax)
+    for i in range(0, nmin):
+    df[ f'int{i}'] = np.random.randint(0, 100,size=(nmax, ))
+    df[ f'flo{i}'] = np.random.rand(1, nmax)[0] 
+    df[ f'str{i}'] =  [ ",".join([ str(t) for t in np.random.randint(10000000,999999999,size=(500, )) ] )  for k in range(0,nmax) ]
+    print(df.head)
+    df.to_parquet('myfile')
+
+
+    #### Pandas version
+    dfp = pd.read_parquet('myfile')
+    i=0
+    dfp['new1'] = dfp.apply(lambda x :  min( x['str1'].split(","))   , axis=1)
+    dfp.groupby([f'int{i}']).agg({'count'})
+    dfp.groupby(['key']).apply(lambda x:";".join(x[f'str{i}'].values))
+    dfp.groupby([f'flo{i}']).agg({'sum'})
+    dfp.to_parquet('myfile.parquet')
+
+
+    ### POLARS Version 
+    df = pl.read_parquet('myfile')
+    i=0
+    df['new1'] = df.select(["*",  pl.col("str1").apply(lambda x : min(x.split(",")) ).alias("booknew")])['booknew']
+    df.groupby(f'int{i}').agg(pl.all().count())
+    df.groupby('key').agg(pl.col(f'str{i}')).select([pl.col('key'), pl.col(f'str{i}').arr.join(",")])
+    df.groupby([f'flo{i}']).agg(pl.all().sum())
+    df.to_parquet('myfile.parquet.polars')
+
+
+def test_create_parquet():
+    nmin = 2
+    nmax=5000
+    # df = pd_create_random(nmax=5000000)
+    df = pd.DataFrame()
+    df['key'] = np.arange(0, nmax)
+    for i in range(0, nmin):
+        df[ f'int{i}'] = np.random.randint(0, 100,size=(nmax, ))
+        df[ f'flo{i}'] = np.random.rand(1, nmax)[0] 
+        df[ f'str{i}'] =  [ ",".join([ str(t) for t in np.random.randint(10000000,999999999,size=(500, )) ] )  for k in range(0,nmax) ]
+    print(df.head)
+    df.to_parquet('ztest/myfile.parquet')
+    return 'ztest/myfile.parquet'
+
+    
